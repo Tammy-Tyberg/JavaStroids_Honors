77f030fa9e5129b8b8bbdde8fcfdfc7c34c29a4b
shternafranklin <shternafranklin@gmail.com>
2015-11-30 00:08:46 -0500

    Update JavaStroidsLauncher.java

--- Documents/workspace/JavaStroids_Honors/src/com/gmail/tamtyberg/desktop/JavaStroidsLauncher.java
+++ Documents/workspace/JavaStroids_Honors/src/com/gmail/tamtyberg/desktop/JavaStroidsLauncher.java
@@ -1,10 +1,9 @@
-package com.gmail.tamtyberg.desktop;
+package com.HCIHonors.javastroid.desktop;
 
+
+import com.HCIHonors.javastroid.Main.MainGame;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
-import com.gmail.tamtyberg.game.Game;
-
-
 
 public class JavaStroidsLauncher {
 	
@@ -12,14 +11,17 @@ public class JavaStroidsLauncher {
 		
 		//Lwjgl object and initializes it
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		 
 		
+		// whether to use OpenGL ES 2.0
+    
 		config.title = "JavaStroids"; //gives title to frame
 		
 		config.width = 800; //frame width
 		
-		config.height = 480;  //frame height
+		config.height = 600;  //frame height
 		
 		//this one line is the whole game
-		new LwjglApplication(new Game(), config); //lgwjglApp with Game passed in and config object
+		new LwjglApplication(new MainGame(), config); //lgwjglApp with Game passed in and config object
 	}
 }
